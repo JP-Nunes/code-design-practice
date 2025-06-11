372641a657f30d5fa6d6f01c756e636c53136e27
jotape <joao.p_nunes@outlook.com>
2025-06-11 19:26:19 -0300

    Setup inicial com Spring Boot

---
+++ src/main/kotlin/br/com/study/code_design_practice/CodeDesignPracticeApplication.kt
@@ -0,0 +1,11 @@
+package br.com.study.code_design_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeDesignPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodeDesignPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/study/code_design_practice/CodeDesignPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.study.code_design_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeDesignPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
